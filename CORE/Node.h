861a574bf46a1c496a2363e8e35123b4890b841b
Alexej <alexej4703@gmail.com>
2017-11-06 22:32:27 +0300

    Second commit

--- CORE/Node.h
+++ CORE/Node.h
@@ -8,22 +8,14 @@ class Node {
 public:
     Node(Vertix*);
 
-    // Изменяет ссылку узла на другой вертикс
     void setVertix(Vertix*);
-    // Добавляет ссылку на связный вертикс
     void addVerix(Vertix*);
-    // Печатает вертикс узла
     void print(void);
-    // Печатает связные вертиксы, выше вертикса узла
     void printUp(void);
-    // Печатает связные вертиксы, ниже вертикса узла
     void printDown(void);
 private:
-    // Вертикс узла
     Vertix *vertix;
-    // Ссылки на связные вертиксы, выше вертикса узла
     std::vector<Vertix*> up;
-    // Ссылки на связные вертиксы, ниже вертикса узла
     std::vector<Vertix*> down;
 };
 
